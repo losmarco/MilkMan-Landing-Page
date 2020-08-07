1c89e0c91031d3ad67fdd710e53ced001ece207e
Marco Lai <laimgmarco@gmail.com>
2020-08-06 20:33:18 -0700

    nav and hero section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   theme: {
     extend: {},
+    screens: {
+      xs: '420px',
+      sm: '640px',
+      md: '768px',
+      ml: '920px',
+      lg: '1024px',
+      xl: '1280px',
+      
+    },
+    zIndex: {
+      '-10': '-10',
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/ui'),
+  ],
 }
